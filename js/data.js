f7ecfaf981a61935ae42c9d99f9300d13ea68e77
Etienne Deneuve <etienne@deneuve.xyz>
2020-02-27 13:17:38 +0100

    Updates

--- js/data.js
+++ js/data.js
@@ -274,11 +274,4 @@
   
   "url": "/2015/12/16/get-nexttrain/",
   "tags" : ""
-},{
-  "title": "Creating an Adaptive Web Site for Multiple Screens",
-  "date" : "Nov 18, 2011",
-  "summary" : "How to build a web site that automatically adapts its layout to various screen sizes, orientations, and resolutions on desktop browsers and mobile devices.",
-  "thumbnail": "/images/courses/adaptive_tn.png",
-  "url": "https://linkedin-learning.pxf.io/c/1252977/449670/8005?u=https%3A%2F%2Fwww.linkedin.com%2Flearning%2Fcreating-an-adaptive-web-site-for-multiple-screens",
-  "tags" : "css,javascript,html,web designer,web developer"
 }]
